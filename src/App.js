756f4cca5ceef48aea3d0aae1232a818bdc988d6
Rustam <mirzamorf@yandex.ru>
2021-02-18 21:34:33 +0300

    base setup styles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import { Button } from '@material-ui/core';
 import './App.css';
 
 function App() {
-  return <Button color="primary">Hello World</Button>;
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  )
 }
 
 export default App;
