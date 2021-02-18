1f24b287aaffd19f40fff970a3524e9c6bf749e0
Rustam <mirzamorf@yandex.ru>
2021-02-18 22:44:30 +0300

    fetch get and post services

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -12,7 +12,7 @@ export default function HomePage() {
 
     return (
         <Container className={classes.root}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa officiis, quaerat quam sunt vel veniam. Animi commodi distinctio earum, eligendi ex facilis nemo nisi nobis pariatur sint unde vitae! Dolor, molestiae, quidem? Accusamus aut doloremque ea iure omnis, porro quam qui quo voluptas voluptatum. Expedita, reiciendis, ullam! Blanditiis fugit odio quaerat. Accusantium aliquam cumque dolorum eum ipsum nihil nostrum odit quae quam veritatis! Maiores reprehenderit repudiandae similique vitae! Alias architecto blanditiis consectetur dolor dolore dolorem ipsum minima, nobis nostrum obcaecati odio pariatur placeat, porro praesentium quibusdam repellat vitae? Ab eligendi ex itaque neque, nulla omnis sunt. Commodi iste quam voluptas?
+
         </Container>
     )
 }
---
+++ src/services/fetch.js
@@ -0,0 +1,42 @@
+const apiBase = 'https://jsonplaceholder.typicode.com'
+const defaultHeaders = {
+    'Content-Type': 'application/json'
+}
+
+class req {
+    constructor(apiBase, defaultHeaders) {
+        this.apiBase = apiBase
+        this.defaultHeaders = defaultHeaders
+    }
+
+    async get(url, headers = {}) {
+        const res = await fetch(`${this.apiBase}/${url}`, {
+            method: 'GET',
+            headers: {
+                ...this.defaultHeaders,
+                ...headers
+            }
+        })
+        return await res.json()
+    }
+
+    async post(url, body, headers) {
+        let data
+        try {
+            data = JSON.stringify(body)
+        } catch(e) {
+            throw new Error(e.message)
+        }
+
+        const res = await fetch(`${this.apiBase}/${url}`, {
+            method: 'POST',
+            body: data,
+            headers: {
+                ...this.defaultHeaders,
+                ...headers
+            }
+        })
+        return await res.json()
+    }
+
+}
